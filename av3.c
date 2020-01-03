f0136779d80f917fa12a9b5a95dbb55a0fcd3c79
Andreas Manuel Korn <andreas.korn98@gmail.com>
2020-01-03 11:29:05 +0100

    He hecho algo

--- av3.c
+++ av3.c
@@ -21,6 +21,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int getFileName(char *args[]);
 static void *thread_start(void* ptr);
+void increment_stack();
 
 typedef struct str_thdata {
     int thread_no;
@@ -36,21 +37,23 @@ int main(int argc, char *argv[]){
         } else {
             stack = my_stack_read(filename);
         }
-        stack = my_stack_init(NUM_THREADS);
+        stack = my_stack_init(INT_SIZE);
         int length = my_stack_len(stack);
         printf("stack->size: %d\n", stack->size);
         printf("initial stack lenght: %d\n", length);
         while(length < NUM_THREADS){
-            int *data=0;
-            my_stack_push(stack, data);
+            int data = 0;
+            my_stack_push(stack, &data);
             length++;
         }
         printf("final stack length: %d\n", my_stack_len(stack));
         
         for(int i=0; i<NUM_THREADS; i++){
             thdata data; strcpy(data.message, "Soy el hilo "); data.thread_no=i;
-            pthread_create(&thread[i], NULL, (void *)thread_start, (void *)&data);
+            pthread_create(&thread[i], NULL, (void *)increment_stack, NULL);
+            //pthread_create(&thread[i], NULL, (void *)thread_start, (void *)&data);
         }
+
         for(int i=0; i<NUM_THREADS; i++){
             pthread_join(thread[i], NULL);
         }
@@ -58,11 +61,34 @@ int main(int argc, char *argv[]){
         printf("Written elements from stack to file: %d\n", my_stack_write(stack, filename));   fflush(stdout);
         //printf("Released bytes: %d\n", my_stack_purge(stack));        fflush(stdout);
         printf("Bye from main\n");
-        //close(fd);
+        close(fd);
         pthread_exit(&mutex);
     } else {
         return EXIT_FAILURE;
     }
+}
+
+void increment_stack(){
+    int *number;
+    for(int i = 0; i < 5 && stack->first; i++){
+        pthread_mutex_lock(&mutex);
+        printf("Soy el hilo %ld ejecutando pop \n", pthread_self());
+        number = my_stack_pop(stack);
+        pthread_mutex_unlock(&mutex);
+        if(number == NULL){
+            pthread_exit(NULL);
+        }
+
+        fprintf(stdout, "numero = %d\n", *number);    fflush(stdout);
+        *number++;
+        fprintf(stdout, "numero = %d\n", *number);    fflush(stdout);
+
+        pthread_mutex_lock(&mutex);
+        printf("Soy el hilo %ld ejecutando push \n", pthread_self());
+        my_stack_push(stack, number);
+        pthread_mutex_unlock(&mutex);
+    }
+    pthread_exit(NULL);
 }
 
 static void *thread_start(void* ptr){
